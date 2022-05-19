ddfb8214265b3c2c557c94d793e27ee651699bff
ultimatedragon111 <ultimatedragon111@gmail.com>
2022-05-19 20:53:06 +0200

    Reproductor feo

---
+++ ReproductorPrueba/app/src/main/java/com/example/reproductorprueba/Song.java
@@ -0,0 +1,17 @@
+package com.example.reproductorprueba;
+
+public class Song {
+    String url;
+
+    public Song(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
